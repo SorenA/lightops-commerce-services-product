16e6f533317d4c9e924736a9b470e2423e763542
SorenA <s.dev@lysdal.org>
2020-07-08 21:19:40 +0200

    Add gRPC services.

---
+++ src/LightOps.Commerce.Services.Product/Domain/Services/V1/HealthGrpcService.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using LightOps.Commerce.Proto.Services.Health.V1;
+using LightOps.Commerce.Services.Product.Api.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+
+namespace LightOps.Commerce.Services.Product.Domain.Services.V1
+{
+    public class HealthGrpcService : Health.HealthBase
+    {
+        private readonly ILogger<HealthGrpcService> _logger;
+        private readonly IHealthService _healthService;
+
+        public HealthGrpcService(
+            ILogger<HealthGrpcService> logger,
+            IHealthService healthService)
+        {
+            _logger = logger;
+            _healthService = healthService;
+        }
+
+        public override async Task<HealthCheckResponse> Check(HealthCheckRequest request, ServerCallContext context)
+        {
+            if (string.IsNullOrEmpty(request.Service))
+            {
+                // Perform overall health-check
+                var statusMap = new Dictionary<string, HealthCheckResponse.Types.ServingStatus>();
+
+                // Check all services
+                statusMap.Add("service.product.v1.ProtoProductService", await GetProductServiceStatusAsync());
+
+                return new HealthCheckResponse
+                {
+                    Status = statusMap.All(x => x.Value == HealthCheckResponse.Types.ServingStatus.Serving)
+                        ? HealthCheckResponse.Types.ServingStatus.Serving
+                        : HealthCheckResponse.Types.ServingStatus.NotServing,
+                };
+            }
+
+            var servingStatus = HealthCheckResponse.Types.ServingStatus.Unknown;
+            switch (request.Service)
+            {
+                case "service.product.v1.ProtoProductService":
+                    servingStatus = await GetProductServiceStatusAsync();
+                    break;
+                default:
+                    throw new RpcException(new Status(StatusCode.NotFound, $"Service {request.Service} not found."));
+            }
+
+            return new HealthCheckResponse
+            {
+                Status = servingStatus,
+            };
+        }
+
+        private async Task<HealthCheckResponse.Types.ServingStatus> GetProductServiceStatusAsync()
+        {
+            return await _healthService.CheckProduct() == HealthStatus.Healthy
+                ? HealthCheckResponse.Types.ServingStatus.Serving
+                : HealthCheckResponse.Types.ServingStatus.NotServing;
+        }
+    }
+}
